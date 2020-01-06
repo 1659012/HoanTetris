3fe147eb49f03fe103d1387f5f079054e7e2ca2a
Hoan Le <lenguyenhanhoan@gmail.com>
2020-01-06 16:35:33 +0700

    dropShape

--- src/components/GameBoard.vue
+++ src/components/GameBoard.vue
@@ -1,5 +1,7 @@
 <template>
-  <div id="gameBoard" ref="gameBoard"></div>
+  <div id="gameBoard" ref="gameBoard">
+    <h3>{{ dropRow }}</h3>
+  </div>
 </template>
 
 <script>
@@ -9,8 +11,23 @@ export default {
   components: {},
   data() {
     return {
-      sizeX: 5,
-      sizeY: 5
+      maxRow: 20,
+      maxCol: 10,
+      currentRow: 0,
+      currentCol: 0,
+      dropRow: 0,
+      shapes: {
+        IShape: [[1, 0], [3, 1], [2, 3], [0, 2]],
+        JShape: [[1, 0], [2, 1], [1, 2], [2, 0]],
+
+        //LShape: [[0, 1, 0], [0, 1, 0], [0, 1, 1]],
+        LShape: [[0, 1, 0], [0, 1, 0], [0, 1, 1]],
+
+        OSquare: [[1, 1, 0], [1, 1, 0], [0, 0, 0]],
+        SShape: [[0, 2], [0, 0], [2, 0], [2, 2]],
+        TShape: [[0, 1], [1, 0], [2, 1], [1, 2]],
+        ZShape: [[0, 1], [1, 0], [2, 1], [1, 2]]
+      }
     };
   },
   mounted() {
@@ -18,7 +35,7 @@ export default {
   },
   computed: {},
   watch: {},
-  methods: {   
+  methods: {
     initialize() {
       new Promise(resolve => {
         this.createNewBoard();
@@ -30,54 +47,118 @@ export default {
 
     createNewBoard() {
       var gameBoard = this.$refs.gameBoard;
-      for (let x = 0; x < this.sizeX; x++) {
-        for (let y = 0; y < this.sizeY; y++) {
+      for (let row = 0; row < this.maxRow; row++) {
+        for (let col = 0; col < this.maxCol; col++) {
           let node = document.createElement("div");
           node.setAttribute("class", "boardCell");
-          node.setAttribute("id", x + "-" + y);
+          node.setAttribute("id", row + "-" + col);
           gameBoard.appendChild(node);
         }
       }
     },
 
-    getRadomCell(horizontal, vertical) {
-      var x = Math.round(Math.random() * horizontal);
-      var y = Math.round(Math.random() * vertical);
-      return { x, y };
+    dropShape() {
+      if (this.dropRow < this.maxRow) {
+        this.dropRow++;
+      } else {
+        this.dropRow = 0;
+      }
     },
-    chageRamdomCell() {
-      let position = this.getRadomCell(this.sizeX - 1, this.sizeY - 1);
-      // console.log(position);
-      var cell = document.getElementById(position.x + "-" + position.y);
-      cell.classList.add("activeCell");
 
-      setTimeout(function() {
+    drawShape(row, col, shapeType) {
+      // //clean previous shape
+      // var cells = document.getElementsByClassName("boardCell activeCell");
+      // cells.forEach(cell => {
+      //   cell.classList.remove("activeCell");
+      // });
+
+      // //render shape
+      // var count = 0;
+      // for (let i = 0; i < 3; i++) {
+      //   for (let j = 0; j < 3; j++) {
+      //     let cellRow = row + i;
+      //     let cellCol = col + j;
+      //     console.log(
+      //       "cell " + cellRow + "-" + cellCol + "= " + this.shapes.LShape[i][j]
+      //     );
+      //     if (me.shapes.LShape[i][j]) {
+      //       count++;
+      //       console.log("count cell=" + count);
+      //       console.log("get active cell " + cellRow + "-" + cellCol);
+      //       let tempCell = document.getElementById(cellRow + "-" + cellCol);
+      //       //debugger
+      //       if (tempCell) tempCell.classList.add("activeCell");
+      //     }
+      //   }
+      // }
+      // console.log("==================================================");
+      let me = this;
+      new Promise(resolve => {
+        me.cleanActiveCell();
+        resolve(true);
+      }).then(() => {
+        me.renderShape(row, col);
+      });
+    },
+    cleanActiveCell() {
+      //clean previous shape
+      var cells = document.getElementsByClassName("boardCell activeCell");
+      cells.forEach(cell => {
         cell.classList.remove("activeCell");
-      }, 1000);
+      });
+    },
+
+    renderShape(row, col) {
+      //render shape
+      var count = 1;
+      for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+          let cellRow = row + i;
+          let cellCol = col + j;
+          console.log(
+            "cell " + cellRow + "-" + cellCol + "= " + this.shapes.LShape[i][j]
+          );
+          if (this.shapes.OSquare[i][j]) {
+           
+            console.log("count cell=" + count);
+            console.log("get active cell " + cellRow + "-" + cellCol);
+            let tempCell = document.getElementById(cellRow + "-" + cellCol);
+            //debugger
+            if (tempCell) tempCell.classList.add("activeCell");
+             count++;
+          }
+        }
+      }
+      console.log("==================================================");
+      
     },
 
     runGame() {
       let me = this;
       setInterval(function() {
-        me.chageRamdomCell();
-      }, 1500);
+        new Promise(resolve => {
+          me.drawShape(me.dropRow, 0, 1);
+          resolve();
+        }).then(() => {
+          me.dropShape();
+        });
+      }, 3000);
     }
   }
 };
 </script>
 
-
-<style >
+<style>
 #gameBoard {
   border: 2px solid black;
-  width: 510px;
-  height: 510px;
+  width: 310px;
+  height: 620px;
 }
 
 .boardCell {
-  border: 1px solid rgb(224, 224, 224);
-  width: 100px;
-  height: 100px;
+  border: 0.5px solid rgb(224, 224, 224);
+  width: 30px;
+  height: 30px;
   display: box;
   float: left;
   /* Firefox */
