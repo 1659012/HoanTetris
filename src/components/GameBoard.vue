0ccaf3277bfc7613ddf8f3fb2f6556766e696438
Hoan Le <lenguyenhanhoan@gmail.com>
2020-01-07 15:54:46 +0700

    listenToKeyDown

--- src/components/GameBoard.vue
+++ src/components/GameBoard.vue
@@ -1,7 +1,5 @@
 <template>
-  <div id="gameBoard" ref="gameBoard">
-    <h3>{{ dropRow }}</h3>
-  </div>
+  <div id="gameBoard" ref="gameBoard"></div>
 </template>
 
 <script>
@@ -16,17 +14,59 @@ export default {
       currentRow: 0,
       currentCol: 0,
       dropRow: 0,
+      dropCol: 3,
+      shapeTypes: [
+        "IShape",
+        "JShape",
+        "LShape",
+        "OShape",
+        "SShape",
+        "TShape",
+        "ZShape"
+      ],
       shapes: {
-        IShape: [[1, 0], [3, 1], [2, 3], [0, 2]],
-        JShape: [[1, 0], [2, 1], [1, 2], [2, 0]],
+        IShape: [
+          [1, 0],
+          [3, 1],
+          [2, 3],
+          [0, 2]
+        ],
+        JShape: [
+          [1, 0],
+          [2, 1],
+          [1, 2],
+          [2, 0]
+        ],
 
-        //LShape: [[0, 1, 0], [0, 1, 0], [0, 1, 1]],
-        LShape: [[0, 1, 0], [0, 1, 0], [0, 1, 1]],
+        LShape: [
+          [0, 1, 0],
+          [0, 1, 0],
+          [0, 1, 1]
+        ],
 
-        OSquare: [[1, 1, 0], [1, 1, 0], [0, 0, 0]],
-        SShape: [[0, 2], [0, 0], [2, 0], [2, 2]],
-        TShape: [[0, 1], [1, 0], [2, 1], [1, 2]],
-        ZShape: [[0, 1], [1, 0], [2, 1], [1, 2]]
+        OShape: [
+          [1, 1, 0],
+          [1, 1, 0],
+          [0, 0, 0]
+        ],
+        SShape: [
+          [0, 2],
+          [0, 0],
+          [2, 0],
+          [2, 2]
+        ],
+        TShape: [
+          [0, 1],
+          [1, 0],
+          [2, 1],
+          [1, 2]
+        ],
+        ZShape: [
+          [0, 1],
+          [1, 0],
+          [2, 1],
+          [1, 2]
+        ]
       }
     };
   },
@@ -42,6 +82,7 @@ export default {
         resolve();
       }).then(() => {
         this.runGame();
+        this.listenToKeyDown();
       });
     },
 
@@ -57,92 +98,91 @@ export default {
       }
     },
 
+    listenToKeyDown() {
+      let me = this;
+      document.onkeydown = function(e) {
+        switch (e.keyCode) {
+          case 32: //space
+            //console.log("space");
+            break;
+
+          case 37: //left
+            //console.log("left");
+            if (me.dropCol > 0) me.dropCol--;
+            me.clearActiveCell();
+            break;
+
+          case 39: //right
+            //console.log("right");
+            if (me.dropCol < me.maxCol) me.dropCol++;
+            me.clearActiveCell();
+            break;
+
+          case 38: //up
+            console.log("up");
+            break;
+
+          case 40: //down
+            console.log("down");
+            break;
+        }
+      };
+    },
+
     dropShape() {
       if (this.dropRow < this.maxRow) {
         this.dropRow++;
-      } else {
+      } else if (this.dropRow >= this.maxRow) {
         this.dropRow = 0;
+        this.drowCol = 3; //format shape to drop center
       }
     },
 
-    drawShape(row, col, shapeType) {
-      // //clean previous shape
-      // var cells = document.getElementsByClassName("boardCell activeCell");
-      // cells.forEach(cell => {
-      //   cell.classList.remove("activeCell");
-      // });
-
-      // //render shape
-      // var count = 0;
-      // for (let i = 0; i < 3; i++) {
-      //   for (let j = 0; j < 3; j++) {
-      //     let cellRow = row + i;
-      //     let cellCol = col + j;
-      //     console.log(
-      //       "cell " + cellRow + "-" + cellCol + "= " + this.shapes.LShape[i][j]
-      //     );
-      //     if (me.shapes.LShape[i][j]) {
-      //       count++;
-      //       console.log("count cell=" + count);
-      //       console.log("get active cell " + cellRow + "-" + cellCol);
-      //       let tempCell = document.getElementById(cellRow + "-" + cellCol);
-      //       //debugger
-      //       if (tempCell) tempCell.classList.add("activeCell");
-      //     }
-      //   }
-      // }
-      // console.log("==================================================");
-      let me = this;
-      new Promise(resolve => {
-        me.cleanActiveCell();
-        resolve(true);
-      }).then(() => {
-        me.renderShape(row, col);
-      });
-    },
-    cleanActiveCell() {
+    drawShape(row, col, shapeType, shapeIndex) {
       //clean previous shape
+      var cells = document.getElementsByClassName("boardCell activeCell");
+      cells.forEach(cell => {
+        cell.classList.remove("activeCell");
+      });
+
+      //render shape
+      for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+          let cellRow = row + i;
+          let cellCol = col + j;
+          //console.log("cell " + cellRow + "-" + cellCol + "= " + this.shapes.LShape[i][j]);
+          if (this.shapes.LShape[i][j]) {
+            //console.log("get active cell " + cellRow + "-" + cellCol);
+            let tempCell = document.getElementById(cellRow + "-" + cellCol);
+            //debugger
+            if (tempCell) tempCell.classList.add("activeCell");
+          }
+        }
+      }
+      //console.log("==================================================");
+    },
+
+    clearActiveCell() {
       var cells = document.getElementsByClassName("boardCell activeCell");
       cells.forEach(cell => {
         cell.classList.remove("activeCell");
       });
     },
 
-    renderShape(row, col) {
-      //render shape
-      var count = 1;
-      for (let i = 0; i < 3; i++) {
-        for (let j = 0; j < 3; j++) {
-          let cellRow = row + i;
-          let cellCol = col + j;
-          console.log(
-            "cell " + cellRow + "-" + cellCol + "= " + this.shapes.LShape[i][j]
-          );
-          if (this.shapes.OSquare[i][j]) {
-           
-            console.log("count cell=" + count);
-            console.log("get active cell " + cellRow + "-" + cellCol);
-            let tempCell = document.getElementById(cellRow + "-" + cellCol);
-            //debugger
-            if (tempCell) tempCell.classList.add("activeCell");
-             count++;
-          }
-        }
-      }
-      console.log("==================================================");
-      
-    },
-
     runGame() {
       let me = this;
       setInterval(function() {
         new Promise(resolve => {
-          me.drawShape(me.dropRow, 0, 1);
+          me.drawShape(me.dropRow, me.dropCol, 1, 1);
           resolve();
         }).then(() => {
           me.dropShape();
+          if (me.dropRow >= me.maxRow) {
+            me.dropRow = 0;
+            me.drowCol = 3; //format shape to drop center
+          }
         });
-      }, 3000);
+      }, 1000);
     }
   }
 };
@@ -153,6 +193,7 @@ export default {
   border: 2px solid black;
   width: 310px;
   height: 620px;
+  z-index: 99999;
 }
 
 .boardCell {
@@ -168,9 +209,10 @@ export default {
   background: gray;
 }
 .activeCell {
-  background-image: url("../assets/images/one-punch-man.png");
+  /* background-image: url("../assets/images/one-punch-man.png");
   background-repeat: no-repeat;
   background-position: 50% 50%;
-  background-size: cover;
+  background-size: cover; */
+  background-color: blue;
 }
 </style>
